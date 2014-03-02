0e08c0fbe69db956b74e35c1af3c779631bbdab1
Brian Story <bstory32@gmail.com>
2014-03-02 16:03:32 -0600

    Added initial files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Commercial Connected</title>
+	<link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+	<h1>Hello world</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, cumque, quas debitis pariatur eaque qui earum animi quis doloremque ipsa saepe repellendus cum! Incidunt, quasi dolorem perferendis quos quia recusandae.</p>
+	
+	<script src="public/js/main.js"></script>
+</body>
+</html>
